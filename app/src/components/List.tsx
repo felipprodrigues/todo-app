e7917402373138a997d3ef75de549aea081c6c28
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-06-06 23:52:31 -0300

    add list styles

--- app/src/components/List.tsx
+++ app/src/components/List.tsx
@@ -1,11 +1,34 @@
+import styles from '../styles/Lists.module.css'
+import { Trash } from 'phosphor-react'
+
 export function List({tasks}) {
   console.log(tasks.length)
+
+
   return (
     <>
       {tasks.map((item: any, index: number) => {
         return (
-          <div>
-            <span key={index + 1}>{item.content}</span>
+          <div className={styles.wrapper}>
+
+            <input
+              id={item.content}
+              name={item.content}
+              type="radio"
+              className={styles.input}
+            />
+            <label
+              className={styles.label}
+              id={item.content}
+            >
+              {item.content}
+            </label>
+
+            <Trash
+              size={20}
+              className={styles.icon}
+            />
+
           </div>
         )
       })}
